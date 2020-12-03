05d54187dbbf9e84367da957e6cc665e59528df8
Jared Henderson <jared@netrivet.com>
2020-12-03 16:14:17 -0500

    convert to typescript, add /cover-props route

---
+++ src/app-audios.ts
@@ -0,0 +1,79 @@
+import '@friends-library/env/load';
+import env from '@friends-library/env';
+import { Handler, APIGatewayEvent } from 'aws-lambda';
+import {
+  allPublishedAudiobooks,
+  jsFriendMap,
+  setResolveMap,
+} from '@friends-library/friends';
+import * as docMeta from '@friends-library/document-meta';
+
+interface Audio {
+  id: string;
+  date: string;
+  title: string;
+  friend: string;
+  reader: string;
+  artwork: string;
+  description: string;
+  shortDescription: string;
+  parts: {
+    audioId: string;
+    index: number;
+    title: string;
+    duration: number;
+    size: number;
+    sizeLq: number;
+    url: string;
+    urlLq: string;
+  }[];
+}
+
+const handler: Handler = async (event: APIGatewayEvent) => {
+  setResolveMap(jsFriendMap);
+  const meta = await docMeta.fetchSingleton();
+  const { CLOUD_STORAGE_BUCKET_URL: CLOUD_URL } = env.require(`CLOUD_STORAGE_BUCKET_URL`);
+  const lang = event.queryStringParameters?.lang === `es` ? `es` : `en`;
+  const audioResources: Audio[] = [];
+  console.log(allPublishedAudiobooks(lang).length);
+  allPublishedAudiobooks(lang).forEach((edition) => {
+    const { audio, document: doc } = edition;
+    const { friend } = doc;
+    if (!audio) throw new Error(`Missing audio`);
+    const edMeta = meta.get(edition.path);
+    if (!edMeta) throw new Error(`Missing edition meta for ${edition.path}`);
+    const audioMeta = edMeta.audio;
+    if (!audioMeta) throw new Error(`Missing audio metadata for ${edition.path}`);
+    audioResources.push({
+      id: `${doc.id}--${edition.type}`,
+      date: audio.added.toString(),
+      title: doc.title,
+      friend: friend.name,
+      reader: audio.reader,
+      artwork: `${CLOUD_URL}/${audio.imagePath}`,
+      description: doc.description,
+      shortDescription: doc.partialDescription || doc.description,
+      parts: audio.parts.map((part, index) => ({
+        audioId: `${doc.id}--${edition.type}`,
+        index,
+        title: part.title,
+        duration: audioMeta.durations[index],
+        size: audioMeta.HQ.parts[index].mp3Size,
+        sizeLq: audioMeta.LQ.parts[index].mp3Size,
+        url: `${CLOUD_URL}/${audio.partFilepath(index, `HQ`)}`,
+        urlLq: `${CLOUD_URL}/${audio.partFilepath(index, `LQ`)}`,
+      })),
+    });
+  });
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': `application/json`,
+      'Access-Control-Allow-Origin': `*`,
+      'Access-Control-Allow-Headers': `*`,
+    },
+    body: JSON.stringify(audioResources),
+  };
+};
+
+export { handler };
