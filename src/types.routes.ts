2b5128a155f9d4e2406da90b1d6408389a5311f8
Jared Henderson <jared@netrivet.com>
2020-12-22 16:01:51 -0500

    rename exported route types

--- src/types.routes.ts
+++ src/types.routes.ts
@@ -1,16 +1,11 @@
 import type {
-  Resource as CoverPropsResource_v1,
-  Route as CoverPropsRoute_v1,
+  Resource as CoverPropsResourceV1,
+  Route as CoverPropsRouteV1,
 } from './routes/cover-props-v1';
 
 import type {
-  Resource as AppAudiosResource_v1,
-  Route as AppAudiosRoute_v1,
+  Resource as AppAudiosResourceV1,
+  Route as AppAudiosRouteV1,
 } from './routes/app-audios-v1';
 
-export {
-  AppAudiosResource_v1,
-  AppAudiosRoute_v1,
-  CoverPropsResource_v1,
-  CoverPropsRoute_v1,
-};
+export { AppAudiosResourceV1, AppAudiosRouteV1, CoverPropsResourceV1, CoverPropsRouteV1 };
