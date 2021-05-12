8eb90ea53e65d7d5e7b1e3d718df84da9e189f07
Jared Henderson <jared@netrivet.com>
2021-05-12 11:42:51 -0400

    add first pass at /editions route

---
+++ src/routes/editions-v1.ts
@@ -0,0 +1,80 @@
+import '@friends-library/env/load';
+import env from '@friends-library/env';
+import { EditionType, Lang } from '@friends-library/types';
+import { allPublishedEditions } from '@friends-library/friends/query';
+import * as docMeta from '@friends-library/document-meta';
+import { RouteGenerator } from '../types.internal';
+import { evaluate, ParserError } from '@friends-library/evaluator';
+import { query, hydrate } from '@friends-library/dpc-fs';
+
+export interface Resource {
+  id: string;
+  documentId: string;
+  type: EditionType;
+  publishedDate: string;
+  documentTitle: string;
+  friendName: string;
+  friendNameSort: string;
+  squareCoverImageUrl: string;
+  documentDescription: string;
+  documentShortDescription: string;
+  numTotalPaperbackPages: number;
+  chapters: Array<{
+    shortTitle: string;
+  }>;
+}
+
+export type Route = Array<Resource>;
+
+const generate: RouteGenerator<Route> = async () => {
+  const meta = await docMeta.fetchSingleton();
+  const enResources = editions(`en`, meta);
+  const esResources = editions(`es`, meta);
+  return {
+    'editions/v1/en': enResources,
+    'editions/latest/en': enResources,
+    'editions/v1/es': esResources,
+    'editions/latest/es': esResources,
+  };
+};
+
+export default generate;
+
+function editions(lang: Lang, meta: docMeta.DocumentMeta): Route {
+  const CLOUD_URL = env.requireVar(`CLOUD_STORAGE_BUCKET_URL`);
+  return allPublishedEditions(lang).map((edition) => {
+    const document = edition.document;
+    const edMeta = meta.get(edition.path);
+    if (!edMeta) throw new Error(`Missing edition meta for ${edition.path}`);
+
+    const [dpc] = query.getByPattern(edition.path);
+    if (!dpc) throw new Error(`Missing dpc for ${edition.path}`);
+    hydrate.asciidoc(dpc, { chapterHeadingsOnly: false });
+
+    try {
+      var evald = evaluate.toPdfSrcHtml(dpc);
+    } catch (err) {
+      if (err instanceof ParserError) {
+        console.log(err.codeFrame);
+        process.exit(1);
+      } else {
+        throw err;
+      }
+    }
+
+    return {
+      id: `${document.id}--${edition.type}`,
+      documentId: document.id,
+      type: edition.type,
+      publishedDate: edMeta.published,
+      documentTitle: document.title,
+      friendName: document.friend.name,
+      friendNameSort: document.friend.alphabeticalName,
+      squareCoverImageUrl: `${CLOUD_URL}/${edition.squareCoverImagePath}`,
+      documentDescription: document.description,
+      documentShortDescription: document.partialDescription ?? document.description,
+      numTotalPaperbackPages: edMeta.paperback.volumes.reduce((acc, vol) => acc + vol),
+      chapters: evald.chapters.map((ch) => ({ shortTitle: ch.shortHeading })),
+    };
+  });
+}
