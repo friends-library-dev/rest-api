697acbc6caeec3bfab06ee55b2a38d4d6530438a
Jared Henderson <jared@netrivet.com>
2021-05-12 16:30:52 -0400

    tweak app-editions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import appAudiosV1 from './app-audios-v1';
 import coverPropsV1 from './cover-props-v1';
-import editionsV1 from './editions-v1';
+import editionsV1 from './app-editions-v1';
 
 export default [appAudiosV1, coverPropsV1, editionsV1];
